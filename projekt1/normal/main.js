54f3e050ad2f6a8c66e741e699146cafe1c2cc45
DarkB0B <miecze123@onet.pl>
2022-10-06 11:07:11 +0200

    1

--- projekt1/normal/main.js
+++ projekt1/normal/main.js
@@ -1,28 +1,42 @@
-let all = querySelectorAll('.input')
+let all = document.querySelectorAll('.input')
 let przeliczBtn = document.querySelector("#przeliczBtn")
+let index = 2
+
 function przelicz()
 {
-all.array.forEach(element => {
-    
+let suma = 0 
+let roznica = 0
+let values = []
+all.forEach(element => {
+    values.push(Number(element.value))
+    suma += Number(element.value)
+    roznica -= Number(element.value)
 });
-const suma = liczba1 + liczba2 + liczba3 + liczba4 
-const roznica = (liczba1 - liczba2 - liczba3 - liczba4)
-const min = Math.min(liczba1, liczba2,  liczba3, liczba4)
-const max = Math.max(liczba1, liczba2,  liczba3, liczba4)
+
+const min = Math.min.apply(Math, values)
+const max = Math.max.apply(Math, values)
 document.querySelector("#suma").innerHTML = suma
 document.querySelector("#roznica").innerHTML = roznica
 document.querySelector("#max").innerHTML = max
 document.querySelector("#min").innerHTML = min
 }
-liczba1.addEventListener('input', przelicz)
-liczba2.addEventListener('input', przelicz)
-liczba3.addEventListener('input', przelicz)
-liczba4.addEventListener('input', przelicz)
+
 przeliczBtn.addEventListener('click', () => 
 {
  przelicz()
 })
 dodajBtn.addEventListener('click', () => 
 {
-    document.createElement("input", )
+   index += 1
+   console.log(index)
+   var newinput = document.createElement("input");
+   newinput.class = "input"
+   newinput.id = "liczba" + index
+   newinput.type = "number";        
+   console.log(newinput)
+   document.getElementById("inputs").appendChild(newinput)
+   
 })
+all.forEach(element => {
+    element.addEventListener('input', przelicz)
+});
