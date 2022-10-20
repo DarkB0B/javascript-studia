1c97ccaae1938832a953d085c6afa625ba8278d0
DarkB0B <miecze123@onet.pl>
2022-10-20 09:14:00 +0200

    projekt 3

--- projekt2/main.js
+++ projekt2/main.js
@@ -6,8 +6,16 @@ for(var i = 0; i < images.length; i++){
     radio.type = "radio"
     radio.id = "radio"
     radio.name = "img_selector"
-    value = i
+    radio.value = i
     document.getElementById('buttons').appendChild(radio)
+}
+
+function addPhoto(id){
+    document.getElementById('photos').innerHTML = " "
+    var imag = document.createElement("img")
+    imag.id = "photo"
+    imag.src = images[id]
+    document.getElementById('photos').appendChild(imag)
 }
 
 nextBtn.addEventListener('click', () => 
@@ -15,11 +23,7 @@ nextBtn.addEventListener('click', () =>
     counter ++
     if(counter >= images.length)
     {counter = 0}
-    document.getElementById('photos').innerHTML = " "
-    var imag = document.createElement("img")
-    imag.id = "photo"
-    imag.src = images[counter]
-    document.getElementById('photos').appendChild(imag)
+    addPhoto(counter)
     
 })
 prevBtn.addEventListener('click', () => 
@@ -27,10 +31,16 @@ prevBtn.addEventListener('click', () =>
     counter --
     if(counter < 0)
     {counter = images.length-1}
-    document.getElementById('photos').innerHTML = " "
-    var imag = document.createElement("img")
-    imag.id = "photo"
-    imag.src = images[counter]
-    document.getElementById('photos').appendChild(imag)
-    
+    addPhoto(counter)
+})
+radio.addEventListener('click', () => {
+    var buttons = document.querySelectorAll("radio")
+    for(var i = 0; i < buttons.length; i++){
+        if(buttons[i].checked){
+            counter = i + 1;
+            console.log("es")
+        }
+    }
+    addPhoto(counter)
+
 })
