877a801168480feaadfde253c2bc2ba7096448b9
DarkB0B <miecze123@onet.pl>
2022-11-03 09:45:22 +0100

    beautiful mess

---
+++ projekt4/main.js
@@ -0,0 +1,36 @@
+import { Note } from "./note";
+let notes = [];
+document.querySelector("#create").addEventListener("click", () => {
+    document.getElementById("sauce").innerHTML = " "
+    const newnote = document.createElement("div");
+    newnote.id = "newnote";
+    //---- Title
+    const newnotetitle = document.createElement("input");
+    newnotetitle.setAttribute("type", "text");
+    newnotetitle.setAttribute("id", "newnotetitle");
+    const titlelabel = document.createElement("label");
+    titlelabel.setAttribute("for", "newnotetitle");
+    titlelabel.innerHTML = "Title: "
+    //---- Note
+    const newnoteinterior = document.createElement("input");
+    newnoteinterior.setAttribute("type", "text");
+    newnoteinterior.setAttribute("id", "newnoteinterior");
+    const interiorlabel = document.createElement("label");
+    interiorlabel.setAttribute("for", "newnoteinterior");
+    interiorlabel.innerHTML = "Note: "
+    //---- Submit
+    const submitbutton = document.createElement("input");
+    submitbutton.setAttribute("value", "Submit")
+    submitbutton.id = "submit";
+    //---- Append
+    newnote.appendChild(titlelabel);
+    newnote.appendChild(newnotetitle);
+    newnote.appendChild(interiorlabel);
+    newnote.appendChild(newnoteinterior);
+    newnote.appendChild(submitbutton);
+    document.getElementById("sauce").appendChild(newnote);
+})
+document.querySelector("#show").addEventListener("click", () => {
+    
+    
+})
