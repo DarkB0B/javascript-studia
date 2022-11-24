9db89a7c3bff0c2c7c5ea1747b7d110012cc2186
DarkB0B <miecze123@onet.pl>
2022-11-24 09:13:43 +0100

    projekt 7 (asyncAdd)

---
+++ projekt7/main.js
@@ -0,0 +1,39 @@
+
+randomData = [];
+for(var i = 0; i < 100; i++) {
+    var randomnumber = Math.floor(Math.random() * 100) + 1;
+    randomData.push(randomnumber);
+}
+
+async function asyncAdd(a,b) {
+    
+    if (!Number .isInteger(a) || !Number .isInteger(b)) {
+      return Promise.reject('Argumenty muszą mieć typ number!')
+    }
+    return new Promise((resolve, reject) => {
+      setTimeout(() =>{
+        resolve(a+b)
+      }, 10)
+    })
+  }
+async function asyncAddAndMeasure(inputData) {
+    performance.mark('start');
+    let result = 0;
+    for(let i = 0; i < inputData.length; i++) {
+        let data = inputData[i];
+        console.log(result + " + " + data);
+        result = await asyncAdd(result, data);
+    }
+    console.log("result: " + result);
+    performance.mark('end');
+    performance.measure('start to end', 'start', 'end')
+    let measure = performance.getEntriesByName('start to end');
+    console.log("time passed: " + measure[0].duration);
+  }
+
+
+asyncAddAndMeasure(randomData);
+
+
+
+
