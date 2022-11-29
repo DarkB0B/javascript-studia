9efcc7cd0a5ea712ea75f958d8fddafcc218b2e5
DarkB0B <miecze123@onet.pl>
2022-11-29 02:52:38 +0100

    Game is finally working

--- projekt6/main.js
+++ projekt6/main.js
@@ -1,13 +1,14 @@
 let container = document.getElementsByClassName("container")[0];
+let timeleft = 60;
 let holes = [];
 let holeId = 0;
 let speedX = 0;
 let speedY = 0;
-let posX = window.innerHeight/2;
-let posY = window.innerWidth/2;
+let posX = (window.innerHeight/2) - 50;
+let posY = (window.innerWidth/2) - 50;
 let alpha;
-
-
+let gameover = false;
+let score = 0;
 
 
 window.addEventListener('deviceorientation', (event) => {
@@ -20,15 +21,21 @@ window.addEventListener('deviceorientation', (event) => {
 
 function reset(){
     container.innerHTML = "";
+    
     holes = [];
     holeId = 0;
-    posX = window.innerHeight/2;
-    posY = window.innerWidth/2;
+    posX = (window.innerHeight/2) - 50;
+    posY = (window.innerWidth/2) - 50 ;
     speedX = 0;
     speedY = 0;
+    timeleft = 60;
+    score = 0;
+    gameover = false;
     generateBall();
     generateHole();
     moveBall();
+    createScoreAndTimer();
+    countDown();
 }
 
  
@@ -62,21 +69,89 @@ function generateBall(){
     console.log("ball has been generated");
 }
 
-reset();
-
 function moveBall() {
 
 
     if (posX + speedX < window.innerWidth - 50 && posX + speedX > 0) {
         posX += speedX;
         ball.style.left = posX + "px";
+        
     }
     if (posY + speedY < window.innerHeight - 50 && posY + speedY > 0) {
         posY += speedY;
         ball.style.top = posY + "px";
+        
     }
-
+    checkCollision();
     
     window.requestAnimationFrame(moveBall)
     
+    
 }
+
+function checkCollision(){
+    for (let i = 0; i < holes.length; i++){
+        let hole = holes[i];
+        let holeX = hole.offsetLeft;
+        let holeY = hole.offsetTop;
+        let holeSize = hole.offsetWidth;
+        let ballX = posX;
+        let ballY = posY;
+        let ballSize = 50;
+        if (ballX + ballSize > holeX && ballX < holeX + holeSize && ballY + ballSize > holeY && ballY < holeY + holeSize){
+            console.log("collision");
+            hole.remove();
+            holes.splice(i, 1);
+            score ++;
+            generateHole();
+            updateScore();
+        }
+    }
+}
+
+function createScoreAndTimer(){
+    const scoredisplay = document.createElement("div");
+    scoredisplay.id = "score";
+    scoredisplay.innerHTML = "Score: " + score;
+    container.appendChild(scoredisplay);
+    const countdown = document.createElement("div");
+    countdown.id = "countdown";
+    container.appendChild(countdown);
+}
+function updateScore(){
+    let scoreDisplay = document.getElementById("score");
+    scoreDisplay.innerHTML = "Score: " + score;    
+}
+//count 60 seconds and then stop the game
+function countDown() {
+    
+    let downloadTimer = setInterval(function () {
+        timeleft--;
+        if (timeleft <= 0) {
+            clearInterval(downloadTimer);
+            
+            container.innerHTML = "";
+            let gameover = document.createElement("div");
+            gameover.id = "gameover";
+            gameover.innerHTML = "Game Over";
+            gameover.style.fontSize = "50px";
+            container.appendChild(gameover);
+            const scoredisplay = document.createElement("div");
+            scoredisplay.id = "score";
+            scoredisplay.innerHTML = "Score: " + score;
+            scoredisplay.style.fontSize = "50px";
+            container.appendChild(scoredisplay);
+            //add reset button
+            const resetButton = document.createElement("button");
+            resetButton.id = "reset";
+            resetButton.innerHTML = "Reset";
+            resetButton.addEventListener("click", reset);
+            container.appendChild(resetButton);
+            
+
+        } else {
+            document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+        }
+    }, 1000);
+}
+reset();
